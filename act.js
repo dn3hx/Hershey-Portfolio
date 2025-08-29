3411615119abd2a6ba55a6376a79f06ff8d29eb2
dn3hx <dalanginhershey@gmail.com>
2025-08-29 12:29:32 +0800

    Initial Commit

---
+++ act.js
@@ -0,0 +1,45 @@
+/*console.log("Pamantasan ng Lungsod ng Maynila")
+
+let value = window.prompt ("Enter a value");
+console.log(value);
+*/
+
+/*prob 2
+let x = 10;
+let y = 5;
+
+console.log(10+5);
+console.log(10-5);
+console.log(10*5);
+console.log(10/5);
+console.log(10%5);
+*/
+
+
+
+
+
+/*
+let num = 78;
+if (num > 10){
+    console.log("The number is greater than 10");
+}
+else{
+    console.log("The number is less than 10");
+}
+*/
+
+
+/*
+for (let i=1; i<=1000; i++) {
+    console.log(i);
+}
+
+*/
+
+function myFunction(){
+    console.log("I have been invoked!");
+
+}
+
+myFunction();
